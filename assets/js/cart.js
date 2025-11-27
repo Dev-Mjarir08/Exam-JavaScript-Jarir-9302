1b890e33b20a8d4f01065639aed5f793bc836763
Dev-Mjarir08 <08mjarir@gmail.com>
2025-11-27 13:47:43 +0530

    EaXam - js

---
+++ assets/js/cart.js
@@ -0,0 +1,120 @@
+import header from "../utils/utils.js"
+
+
+
+let cartProduct = document.querySelector('#viewCart');
+let cartMsg = document.getElementById('cartMsg')
+let cartData = JSON.parse(localStorage.getItem("cartData")) || []
+let cartTotal = document.querySelector('#cartTotal');
+
+
+const displayData = () => {
+  cartProduct.innerHTML = "";
+    cartMsg.innerHTML = ""
+
+      if (cartData.length === 0) {
+        document.getElementById("totalCol").style.display = "none";
+    cartMsg.innerHTML = `
+        <div class="card-text ">
+        <h3 class="text-center  mt-5 text-muted card-msg">Your Cart is Empty</h3>
+        </div>
+    `;
+    return;
+  }
+
+    cartData.forEach((product) => {
+        const { ProductUrl, ProductName, ProductPrice ,id , qty} = product;
+
+        let col = document.createElement('div')
+        col.classList.add('col-md-8')
+        col.innerHTML = `
+            <div class = "border border-1 p-4 mb-5 rounded shadow">
+                <div class="cart-content d-flex mt-5">
+                <img src="${ProductUrl}" alt="${ProductName}" class="me-3" width="120px" height="120px">
+
+                <p class="fw-semibold text-light me-3">${ProductName}</p>
+                <p class="fw-semibold text-light me-3">₹${ProductPrice}</p>
+              </div>
+              <div class="btns d-flex justify-content-center align-items-center mb-5">              
+              <button class="btn btn-outline-light me-2 fs-5 py-1 px-3" onclick ="decrease(${id})">-</button>
+              <span class="text-light">${qty}</span>
+              <button class="btn btn-outline-light ms-2 fs-5 py-1 px-3" onclick ="increase(${id})">+</button>
+              </div>
+              <button class="btn btn-outline-light" onclick = "removeItem(${id})";>Remove Product</button>
+            </div>
+            
+            
+             
+        `
+        cartProduct.appendChild(col)
+    })
+    calculateProductTotal()
+}
+
+
+const calculateProductTotal = () =>{
+  
+    let totalItems = 0;
+  let totalPrice = 0;
+
+  cartData.forEach((product) => {
+    totalItems += product.qty;
+    totalPrice += product.qty * product.ProductPrice;
+  });
+
+ cartTotal.innerHTML = `
+    <h4 class="title">Order Summary</h4>
+
+    <div class="row-line">
+        <span>Total Items</span>
+        <span>${totalItems}</span>
+    </div>
+
+    <div class="row-line">
+        <span>Subtotal</span>
+        <span>₹${totalPrice}</span>
+    </div>
+
+    <hr class="divider">
+
+    <div class="row-line total-row">
+        <span>Total</span>
+        <span class="total-amount">₹${totalPrice}</span>
+    </div>
+`;
+
+}
+
+
+
+const increase = (id) => {
+  let index = cartData.findIndex((val) => val.id == id);
+  if (index !== -1) {
+    cartData[index].qty++;
+  }
+  localStorage.setItem("cartData", JSON.stringify(cartData));
+  displayData();
+};
+
+const decrease = (id) => {
+    let index = cartData.findIndex(val => val.id == id)
+
+  if (index !== -1 && cartData[index].qty > 1) {
+    cartData[index].qty--;
+}
+    localStorage.setItem("cartData", JSON.stringify(cartData))
+    displayData();
+}
+
+const removeItem = (id) => {
+    cartData = cartData.filter(val => val.id != id);
+    localStorage.setItem("cartData", JSON.stringify(cartData))
+    displayData();
+}
+window.removeItem = removeItem;
+
+
+window.increase = increase;
+window.decrease = decrease;
+
+displayData();
---
+++ assets/utils/utils.js
@@ -0,0 +1,59 @@
+let bas_url = window.location.href;
+let preFix = "";
+
+if (bas_url.includes("vercel.app")) {
+    preFix = "/assets/pages/";
+} 
+else if (bas_url.includes("index")) {
+    preFix = "./assets/pages/";
+} 
+else {
+    preFix = "";
+}
+let nav = `
+    <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><i class="bi bi-lightning"></i>NEO<span>MART</span></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active text-danger" aria-current="page" href="${preFix}/index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="${preFix}addpro.html">Add Product</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="${preFix}editpro.html">Edit Product</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="${preFix}viewpro.html">View Product</a>
+                        </li>
+                </div>
+
+                <div class="icons">
+                 <ul class="navbar-nav d-flex align-items-center">
+                     <li class="nav-item">
+                          <a class="nav-link fw-semibold " href="${preFix}cart.html">
+                    <i class="bi bi-cart btn btn-outline fs-5 text-light "></i>
+             
+                    </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light active" aria-current="page" href="#"> <i class="bi bi-person me-3"></i>Account</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    `
+const header = document.querySelector('header').innerHTML = nav;
+export default header;
+
+
+
+
+
